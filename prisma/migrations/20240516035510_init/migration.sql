44b9cf5100e1cd4c9233cf2b56174378b7a097e8
Thony Silva <thonysilva1993@gmail.com>
2024-05-15 22:55:28 -0500

    CRUD projects

---
+++ prisma/migrations/20240516035510_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Experience" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "level" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Experience_pkey" PRIMARY KEY ("id")
+);
